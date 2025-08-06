9c66583042392e3eea307019986b7bc2a60a6815
sagar <saagarworkmail@gmail.com>
2025-08-06 18:53:01 +0530

    added script to push images

---
+++ dockerpush.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# This is a simple script to push your docker images to docker hub.
+
+DOCKER_USERNAME="saagarsh"
+
+IMAGES=("react-3-tier-application-frontend"
+        "react-3-tier-application-backend"
+        "mysql")
+
+echo "Logging into Docker hub..."
+
+for image in "${IMAGES[@]}";
+do
+docker tag ${image}:latest ${DOCKER_USERNAME}/${image}:1.0
+
+echo "Pushing images to Docker hub..."
+
+docker push ${DOCKER_USERNAME}/${image}:1.0
+
+echo "Succcessfully pushed image: ${DOCKER_USERNAME}/${image}:1.0}"
+
+done
+
+if [ $? -eq 0 ]; then
+    echo "All images pushed successfully."
+else
+    echo "Error occured."
+fi
